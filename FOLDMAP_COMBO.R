d60cf6e9d3534b3ddacbf4dde9c7688df278c5d9
jburnsuoft <87782298+jburnsuoft@users.noreply.github.com>
2021-11-25 14:41:36 -0500

    Update FOLDMAP_COMBO.R

--- FOLDMAP_COMBO.R
+++ FOLDMAP_COMBO.R
@@ -1,227 +1,9 @@
 setwd("~/icSHAPE_Project")
 
-plFolder <- "ADAR_RNAplfold//"
-vitroShapeFolder <- "ADAR_22_SHAPE_vitro_20_0//"
-vivoShapeFolder <- "ADAR_22_SHAPE_VIVO_20_0//"
-
-plFolder <- "ZRANB2_RNAplfold/"
-vitroShapeFolder <- "ZRANB2_SHAPE_VITRO_20_0//"
-vivoShapeFolder <- "ZRANB2_SHAPE_VIVO_20_0//"
-
-plFolder <- "ZRANB2_RNAplfold/"
-vitroShapeFolder <- "ZRANB2_SHAPE_VITRO_20_0//"
-vivoShapeFolder <- "ZRANB2_SHAPE_VIVO_20_0//"
-
-plFolder <- "SRSF1_plfold//"
-vitroShapeFolder <- "SRSF1_A_SHAPE_VITRO_20_0//"
-vivoShapeFolder <- "SRSF1_A_SHAPE_VIVO_20_0//"
-
-plFolder <- "SP1_A_vitro_plfold/"
-vitroShapeFolder <- "SP1_A_SHAPE_VITRO_20_0//"
-vivoShapeFolder <- "SP1_A_SHAPE_VIVO_20_0//"
-
-plFolder <- "WT1_plfold//"
-vitroShapeFolder <- "WT1_A_SHAPE_VITRO_20_0//"
-vivoShapeFolder <- "WT1_A_SHAPE_VIVO_20_0//"
-
-plFolder <- "MAZ_plfold//"
-vitroShapeFolder <- "MAZ_A_SHAPE_VITRO_20_0//"
-vivoShapeFolder <- "MAZ_A_SHAPE_VIVO_20_0//"
-
-plFolder <- "WT1_plfold_100//"
-vitroShapeFolder <- "WT1_B_100_SHAPE_VITRO_50_0//"
-vivoShapeFolder <- "WT1_B_100_SHAPE_VIVO_50_0//"
-
-plFolder <- "ADAR_plfold_100//"
-vitroShapeFolder <- "ADAR_50_SHAPE_vitro_20_0//"
-vivoShapeFolder <- "ADAR_50_SHAPE_VIVO_20_0//"
-
-plFolder <- "ZRANB2_plfold_100/"
-vitroShapeFolder <- "ZRANB2_100_A_SHAPE_VITRO_20_0//"
-vivoShapeFolder <- "ZRANB2_100_A_SHAPE_VIVO_20_0//"
-
-plFolder <- "SRSF1_plfold_100_VITRO//"
-vitroShapeFolder <- "SRSF1_A_100_SHAPE_VITRO_20_0//"
-vivoShapeFolder <- "SRSF1_A_100_SHAPE_VIVO_20_0//"
-
-plFolder <- "ZNF121_PLFOLD_100//"
-vitroShapeFolder <- "ZNF121_A_100_SHAPE_VITRO_20_0//"
-vivoShapeFolder <- "ZNF121_A_100_SHAPE_VIVO_20_0//"
-
-plFolder <- "MAZ_A_PLFOLD_100///"
-vitroShapeFolder <- "MAZ_A_100_SHAPE_VITRO_20_0//"
-vivoShapeFolder <- "MAZ_A_100_SHAPE_VIVO_20_0//"
-
-plFolder <- "METTL3_B_PLFOLD_100/"
-vitroShapeFolder <- "METTL3_B_100_SHAPE_VITRO_20_0//"
-vivoShapeFolder <- "METTL3_B_100_SHAPE_VIVO_20_0//"
-pname <- "METTL3_100"
-
-plFolder <- "YY1_B_PLFOLD_100/"
-vitroShapeFolder <- "YY1_B_100_SHAPE_VITRO_20_0//"
-vivoShapeFolder <- "YY1_B_100_SHAPE_VIVO_20_0//"
-pname <- "YY1_100"
-
-plFolder <- "SP1_A_PLFOLD_100/"
-vitroShapeFolder <- "SP1_A_100_SHAPE_VITRO_20_0//"
-vivoShapeFolder <- "SP1_A_100_SHAPE_VIVO_20_0//"
-pname <- "SP1_100"
-
-plFolder <- "ZBTB48_A_PLFOLD_100//"
-vitroShapeFolder <- "ZBTB48_A_100_SHAPE_VITRO_20_0//"
-vivoShapeFolder <- "ZBTB48_A_100_SHAPE_VIVO_20_0//"
-pname <- "ZBTB48_100"
-
-plFolder <- "CTCF_A_PLFOLD_100//"
-vitroShapeFolder <- "CTCF_A_100_SHAPE_VITRO_20_0//"
-vivoShapeFolder <- "CTCF_A_100_SHAPE_VIVO_20_0//"
-pname <- "CTCF_100"
-
-
-#35 (70)
-
-plFolder <- "ZRANB2_35_PLFOLD///"
-vitroShapeFolder <- "ZRANB2_35_SHAPE_VITRO_20_0///"
-vivoShapeFolder <- "ZRANB2_35_SHAPE_VIVO_20_0///"
-pname <- "ZRANB2_35"
-
-plFolder <- "ADAR_35_PLFOLD////"
-vitroShapeFolder <- "ADAR_35_SHAPE_VITRO_20_0////"
-vivoShapeFolder <- "ADAR_35_SHAPE_VIVO_20_0////"
-pname <- "ADAR_35"
-
-plFolder <- "WT1_B_35_PLFOLD////"
-vitroShapeFolder <- "WT1_B_35_SHAPE_VITRO_20_0////"
-vivoShapeFolder <- "WT1_B_35_SHAPE_VIVO_20_0////"
-pname <- "WT1_35"
-
-plFolder <- "YY1_B_35_PLFOLD////"
-vitroShapeFolder <- "YY1_B_35_SHAPE_VITRO_20_0////"
-vivoShapeFolder <- "YY1_B_35_SHAPE_VIVO_20_0////"
-pname <- "YY1_35"
-
-plFolder <- "SRSF1_A_35_PLFOLD//////"
-vitroShapeFolder <- "SRSF1_35_SHAPE_VITRO_20_0///////"
-vivoShapeFolder <- "SRSF1_35_SHAPE_VIVO_20_0//////"
-pname <- "SRSF1_35"
-
-plFolder <- "CTCF_A_35_PLFOLD/////"
-vitroShapeFolder <- "CTCF_A_35_SHAPE_VITRO_20_0/////"
-vivoShapeFolder <- "CTCF_A_35_SHAPE_VIVO_20_0/////"
-pname <- "CTCF_35"
-
-plFolder <- "SP1_A_35_PLFOLD//////"
-vitroShapeFolder <- "SP1_A_35_SHAPE_VITRO_20_0//////"
-vivoShapeFolder <- "SP1_A_35_SHAPE_VIVO_20_0//////"
-pname <- "SP1_35"
-
-
-############### 150
-plFolder <- "SP1_150_PLFOLD///////"
-vitroShapeFolder <- "SP1_A_75_SHAPE_VITRO_20_0///////"
-vivoShapeFolder <- "SP1_A_75_SHAPE_VIVO_20_0///////"
-pname <- "SP1_75"
-
-############### 10
-plFolder <- "SP1_10_PLFOLD////////"
-vitroShapeFolder <- "SP1_A_10_SHAPE_VITRO_20_0////////"
-vivoShapeFolder <- "SP1_A_10_SHAPE_VIVO_20_0////////"
-pname <- "SP1_10"
-
-plFolder <- "STAU2_50_plfold/"
-vitroShapeFolder <- "STAU2_SHAPE_VITRO_20_0/"
-vivoShapeFolder <- "STAU2_plus_SHAPE_VITRO_20_0/"
-pname <- "STAU2"
-
-############### NOSHAPE
-
-plFolder <- "SRSF1_FULL_PLFOLD"
-pname <- "SRSF1_FULL_PLFOLD"
-
-#PLUS
-vitroShapeFolder <- "SRSF1_A_100_SHAPE_VITRO_20_0_PLUS/"
-vivoShapeFolder <- "SRSF1_A_100_SHAPE_VIVO_20_0_PLUS/"
-pname <- "SRSF1_A_100_PLUS"
-
-#MINUS
-vitroShapeFolder <- "SRSF1_A_100_SHAPE_VITRO_20_0_MINUS/"
-vivoShapeFolder <- "SRSF1_A_100_SHAPE_VIVO_20_0_MINUS/"
-pname <- "SRSF1_A_100_MINUS"
-
-#YY11
-#PLUS
-vitroShapeFolder <- "YY1_B_100_SHAPE_VITRO_PLUS/"
-vivoShapeFolder <- "YY1_B_100_SHAPE_VIVO_PLUS/"
-pname <- "YY1_B_100_PLUS"
-
-#MINUS
-vitroShapeFolder <- "YY1_B_100_SHAPE_VITRO_MINUS/"
-vivoShapeFolder <- "YY1_B_100_SHAPE_VIVO_MINUS/"
-pname <- "YY1_B_100_MINUS"
-
-#ADAR
-#PLUS
-vitroShapeFolder <- "ADAR_50_SHAPE_VITRO_PLUS/"
-vivoShapeFolder <- "ADAR_50_SHAPE_VIVO_PLUS/"
-pname <- "ADAR_100_PLUS"
-
-#MINUS
-vitroShapeFolder <- "ADAR_50_SHAPE_VITRO_MINUS/"
-vivoShapeFolder <- "ADAR_50_SHAPE_VIVO_MINUS/"
-pname <- "ADAR_100_MINUS"
-
-#WT1
-#PLUS
-vitroShapeFolder <- "WT1_50_SHAPE_VITRO_PLUS//"
-vivoShapeFolder <- "WT1_50_SHAPE_VIVO_PLUS//"
-pname <- "WT1_100_PLUS"
-
-#MINUS
-vitroShapeFolder <- "WT1_50_SHAPE_VITRO_MINUS//"
-vivoShapeFolder <- "WT1_50_SHAPE_VIVO_MINUS//"
-pname <- "WT1_100_MINUS"
-
-#200906_1
-plFolder <- "S200906_1_thUni_merged.bam.crosslink_sites_NOV25_50_NEW_plfold"
-vitroShapeFolder <- "S200906_1_thUni_merged.bam.crosslink_sites_NOV25_SHAPE_VITRO_50_NEW_0/"
-vivoShapeFolder <- "S200906_1_thUni_merged.bam.crosslink_sites_NOV25_SHAPE_VIVO_50_NEW_0/"
-pname <- "ADAR_100"
-
-#SP1_NEW  
-plFolder <- "SP1_MERGED_MINUS_NEW_50_PLFOLD"
-vitroShapeFolder <- "SP1_MERGED_MINUS_NEW_50_SEP16_SHAPE_VITRO_20_0//"
-vivoShapeFolder <- "SP1_MERGED_MINUS_NEW_50_SEP16_SHAPE_VIVO_20_0//"
-pname <- "SP1_MINUS_NEW plFOLD"
-
-
-plFolder <- "GTF3A-55mer"
-vitroShapeFolder <- "SP1_A_75_SHAPE_VITRO_20_0///////"
-vivoShapeFolder <- "SP1_A_75_SHAPE_VIVO_20_0///////"
-pname <- "SP1_75"
-plFolder <- "YKT6_PLFOLDING/"
-
-
-#ADAR_UNKNOWN_2
-plFolder <- "ADAR_5_plfold"
-vitroShapeFolder <- "200906_2_thUni_merged.bam.crosslink_sites_NOV25_FULL.bam.crosslink_sites_NOV25_SHAPE_VITRO_20_20_0"
-vivoShapeFolder <- "200906_2_thUni_merged.bam.crosslink_sites_NOV25_FULL.bam.crosslink_sites_NOV25_SHAPE_VIVO_20_20_0/"
-pname <- "ADAR1"
-
-#YY1.A.WINPUT
-#plFolder <- "YY1.A.WINPUT.sorted.100.top500"
-#pname <- "YY1.A.WINPUT"
-
-#ZNF449_OLD
-plFolder <- 
-vitroShapeFolder <- "ZNF449.bam.bed.OLD_FULL_SHAPE_VITRO_20_0/"
-vivoShapeFolder <- "ZNF449.bam.bed.OLD_FULL_SHAPE_VIVO_20_0//"
-pname <- "ZNF449"
-
-#ZNF449_NEW
-plFolder <- 
-vitroShapeFolder <- "ZNF449.bam.bed_FULL_SHAPE_VITRO_20_0/"
-vivoShapeFolder <- "ZNF449.bam.bed_FULL_SHAPE_VIVO_20_0///"
-pname <- "ZNF449"
+plFolder <- " "
+vitroShapeFolder <- " "
+vivoShapeFolder <- " "
+pname <- " "
 
 
 loadplfold <- function(shapePath) {
@@ -281,7 +63,7 @@ plcrosslink <- plMatrix[,26]
 write.table(plcrosslink, sep = "\t", file = paste(pname,"_plcrosslink.txt", sep=""), quote = FALSE)
 
 #MEDIAN
-plColAvg <- apply(plMatrix, 2, mean)
+plColAvg <- apply(plMatrix, 2, median)
 vitroColAvg <- apply(vitroShapeMatrix, 2, median)
 vivoColAvg <- apply(vivoShapeMatrix, 2, median)
 set.seed(1)
@@ -294,7 +76,7 @@ plot(c(0, length(plColAvg)), c(0, 1), type="n", xlab="Position",
 lines(1:length(plColAvg), plColAvg, col = "gray")
 lines(1:length(vitroColAvg), vitroColAvg, col = "blue")
 lines(1:length(vivoColAvg), vivoColAvg, col = "orange")
-title(paste(pname,"RNAplfold"))
+title(paste(pname,"Structure Profile - Median"))
 legend("topleft", c("RNAplFold", "vitro", "vivo"), col = c("gray", "blue", "orange"), lty=1:2, cex=0.5)
 dev.off()
 
@@ -305,12 +87,14 @@ vivoColAvg <- apply(vivoShapeMatrix, 2, mean)
 set.seed(1)
 background <- sample(vitroColAvg)
 
-plot(c(0, 101), c(0, 1), type="n", xlab="Position",
+jpeg(file=paste(pname, "plot_median.jpeg", sep = ""), width = 463, height = 463 )
+plot(c(0, length(plColAvg)), c(0, 1), type="n", xlab="Position",
      ylab="Unpaired Probability" )
 
-lines(1:71, plColAvg, col = "gray")
-lines(1:101, vitroColAvg, col = "blue")
-lines(1:101, vivoColAvg, col = "orange")
-title(paste(pname,"RNAplfold"))
+lines(1:length(plColAvg), plColAvg, col = "gray")
+lines(1:length(vitroColAvg), vitroColAvg, col = "blue")
+lines(1:length(vivoColAvg), vivoColAvg, col = "orange")
+title(paste(pname,"Structure Profile - Mean"))
 legend("topleft", c("RNAplFold", "vitro", "vivo"), col = c("gray", "blue", "orange"), lty=1:2, cex=0.5)
+dev.off()
 
